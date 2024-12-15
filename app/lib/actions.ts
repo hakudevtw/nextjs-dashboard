8150b3ac1eef2d4d1912a3227b8eae0d7e4bdd83
Haku <hakudevtw@gmail.com>
2024-12-15 19:49:13 +0800

    feat: remove not used var

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -122,7 +122,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
   redirect("/dashboard/invoices");
 }
 
-export async function deleteInvoice(id: string, formData: FormData) {
+export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");

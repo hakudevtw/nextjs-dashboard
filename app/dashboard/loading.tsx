7c2e9e25921875ad8c9ed6d4dab6e3121de8ed3b
Haku <hakudevtw@gmail.com>
2024-12-15 12:36:16 +0800

    feat: add page load

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,8 @@
+// loading.tsx is a special Next.js file built on top of Suspense, it allows you to create fallback UI to show as a replacement while page content loads
+// Since <SideNav> is static, it's shown immediately. The user can interact with <SideNav> while the dynamic content is loading
+// The user doesn't have to wait for the page to finish loading before navigating away (this is called interruptable navigation).
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
